5fb1bd3854a6375151683a8d66140711bae11ced
Hnoianko Bohdan <gnoianko@gmail.com>
2017-11-07 01:34:41 +0200

    Homework6.1.1

---
+++ Homework6.1.1/src/com/gmail/gnoianko/Main.java
@@ -0,0 +1,26 @@
+package com.gmail.gnoianko;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[15];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 25);
+        }
+        System.out.println(Arrays.toString(arr));
+        System.out.println(maxNumber(arr));
+
+    }
+
+    public static int maxNumber(int[] arr) {
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+}
