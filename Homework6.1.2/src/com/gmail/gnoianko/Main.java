87a684b826ff2c6532a1cfd66cc085e95f2c1b51
Hnoianko Bohdan <gnoianko@gmail.com>
2017-11-07 01:35:49 +0200

    Homework6.1.2

---
+++ Homework6.1.2/src/com/gmail/gnoianko/Main.java
@@ -0,0 +1,12 @@
+package com.gmail.gnoianko;
+
+public class Main {
+    public static void main (String[]args){
+        System.out.println(string(5,2.3," Hello World"));
+
+    }
+    public static String string(int a, double b, String c){
+        String d = a+b+c;
+        return d;
+    }
+}
